4fb851104e5fd60387e297800b102e515ece7282
Eder <ederyepe777@gmail.com>
2026-09-11 19:31:30 -0700

    Stylize change

--- js/app.js
+++ js/app.js
@@ -2,6 +2,22 @@ let zipMap = {};
 let allCounties = [];
 let allStates = [];
 
+const STATE_NAMES = {
+  AL: 'Alabama', AK: 'Alaska', AZ: 'Arizona', AR: 'Arkansas', CA: 'California',
+  CO: 'Colorado', CT: 'Connecticut', DE: 'Delaware', DC: 'District of Columbia',
+  FL: 'Florida', GA: 'Georgia', HI: 'Hawaii', ID: 'Idaho', IL: 'Illinois',
+  IN: 'Indiana', IA: 'Iowa', KS: 'Kansas', KY: 'Kentucky', LA: 'Louisiana',
+  ME: 'Maine', MD: 'Maryland', MA: 'Massachusetts', MI: 'Michigan',
+  MN: 'Minnesota', MS: 'Mississippi', MO: 'Missouri', MT: 'Montana',
+  NE: 'Nebraska', NV: 'Nevada', NH: 'New Hampshire', NJ: 'New Jersey',
+  NM: 'New Mexico', NY: 'New York', NC: 'North Carolina', ND: 'North Dakota',
+  OH: 'Ohio', OK: 'Oklahoma', OR: 'Oregon', PA: 'Pennsylvania',
+  PR: 'Puerto Rico', RI: 'Rhode Island', SC: 'South Carolina',
+  SD: 'South Dakota', TN: 'Tennessee', TX: 'Texas', UT: 'Utah',
+  VT: 'Vermont', VA: 'Virginia', WA: 'Washington', WV: 'West Virginia',
+  WI: 'Wisconsin', WY: 'Wyoming',
+};
+
 let currentMode = 'before';
 let timerHandle = null;
 let timerStart = 0;
@@ -46,12 +62,15 @@ async function loadData() {
 
   els.countyTotalCount.textContent = allCounties.length.toLocaleString();
 
-  allStates.forEach((s) => {
-    const opt = document.createElement('option');
-    opt.value = s;
-    opt.textContent = s;
-    els.stateInput.appendChild(opt);
-  });
+  allStates
+    .map((abbr) => ({ abbr, name: STATE_NAMES[abbr] || abbr }))
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .forEach(({ abbr, name }) => {
+      const opt = document.createElement('option');
+      opt.value = abbr;
+      opt.textContent = name;
+      els.stateInput.appendChild(opt);
+    });
 
   populateSelect(els.countySelectBefore, allCounties, 'Choose a county\u2026');
 }
